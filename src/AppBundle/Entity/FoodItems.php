14ee41ea7fdba4fd80afeaf2ca2f24ec35ce8ffb
Catalina <cri_catalina@yahoo.com>
2016-05-03 20:05:50 +0300

    add entity folder

---
+++ src/AppBundle/Entity/FoodItems.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * FoodCategory
+ *
+ * @ORM\Table(name="food_item")
+ * @ORM\Entity
+ * @UniqueEntity("name")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class FoodItems {
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * 
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="code", type="integer")
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string")
+     */
+    private $description;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dat_cre", type="datetime")
+     */
+    private $datCre;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dat_upd", type="datetime")
+     */
+    private $datUpd;
+
+}
